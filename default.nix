b6bec0c9aa08635a34a11eee24251168d425960a
bndlfm <firefliesandlightningbugs@gmail.com>
2024-12-19 13:35:03 -0600

    uses upstream OpenXR_SDK and patches the .pc.in with sed, correctly wraps libglvnd libuuid

--- default.nix
+++ default.nix
@@ -1,5 +1,8 @@
 { pkgs, lib }:
 let
+
+  GL = "GLVND"; # or "LEGACY";
+
   bullet' = pkgs.bullet.overrideDerivation (old: {
     cmakeFlags = (old.cmakeFlags or [ ]) ++ [
       "-Wno-dev"
@@ -9,33 +12,7 @@ let
     ];
   });
 
-  GL = "GLVND"; # or "LEGACY";
-
   qtMkDerivation = pkgs.libsForQt5.callPackage ({ mkDerivation }: mkDerivation) {};
-
-  openxr-sdk = pkgs.stdenv.mkDerivation {
-    pname = "openxr-sdk";
-    version = "1.1.43";
-    src = pkgs.fetchFromGitHub {
-      owner = "KhronosGroup";
-      repo = "OpenXR-SDK";
-      rev = "b7ada0bdecd9830f27c2221dad6f0bb933c64f15";
-      hash = "sha256-YsT6z0uymEF35US1ux7E4JOAK6YeOzryulYVGRi0EjA=";
-    };
-    patches = [
-      ./openxr.pc.in.patch
-    ];
-    dontBuild = true;
-  };
-
-
-
-  #openxr-sdk = fetchFromGitHub {
-  #  owner = "bndlfm";
-  #  repo = "OpenXR-SDK_4_OpenMW-VR_Nix";
-  #  rev = "d417ad122a04c1a6d191928170aa1a6368229678";
-  #  hash = "sha256-5tbOsvMWVvWZUl48QtqBJhVqpUMk3WzDY5lhxXx7l0w=";
-  #};
 
 in
 
@@ -46,8 +23,8 @@ in
     src = pkgs.fetchFromGitLab {
       owner = "madsbuvi";
       repo = "openmw";
-      rev = "770584c5112e46be1a00b9e357b0b7f6b449cac5";
-      hash = "sha256-C8lFjKIdbHyvRcZzJNUj8Lif9IqNvuYURwRMpb4sxiQ=";
+      rev = "cac0667193434af3a419d3b14029d2956cf46f5d";
+      hash = "sha256-oAr3Ii8ph8MbsxhLT3Q8W/rjdDvEDo5EuCFkIbzg+UI=";
     };
 
     postPatch = /*sh*/ ''
@@ -71,7 +48,6 @@ in
       mygui
       openal
       openscenegraph
-      openxr-loader
       recastnavigation
       SDL2
       unshield
@@ -91,26 +67,23 @@ in
       cmake
       pkg-config
       python3
-      pkgs.libsForQt5.wrapQtAppsHook
+      libsForQt5.wrapQtAppsHook
     ];
-
-
-    #preBuild = /*sh*/ ''
-      ### COPY BROKEN PC.IN TO BUILD DIRECTORY
-       # echo "Making ${sdkBldPath}"
-       # mkdir -p ${sdkBldPath}
-
-       # echo "Copying openxr-sdk git source to ${sdkBldPath}"
-       # cp -v "${openxr-sdk}" "${sdkBldPath}"
-
-       # ### PATCH $NIX_BUILD_TOP/openxr-sdk/src/loader/openxr.pc.in TO REMOVE ''${exec_prefix}/@CMAKE_INSTALL_LIBDIR TO @CMAKE_INSTALL_FULL_LIBDIR
-       # echo "Patching ${sdkBldPath}/src/loader/openxr.pc.in"
-       # sed -i 's|libdir=\''${exec_prefix}/@CMAKE_INSTALL_LIBDIR@|libdir=@CMAKE_INSTALL_FULL_LIBDIR@|' ${sdkBldPath}/src/loader/openxr.pc.in
-    #'';
 
     enableParallelBuilding = true;
 
-    cmakeFlags = [
+    cmakeFlags = let
+      openxr-sdk = pkgs.fetchFromGitHub {
+        owner = "KhronosGroup";
+        repo = "OpenXR-SDK";
+        rev = "b7ada0bdecd9830f27c2221dad6f0bb933c64f15";
+        hash = "sha256-Aa4Mok1oXQKbj85spoNDnM93pqaSRrvcRuATsyUCOCw=";
+        postFetch = /*sh*/ ''
+          # Patch the openxr.pc.in file in the copied directory
+          sed -i 's|libdir=\''${exec_prefix}/@CMAKE_INSTALL_LIBDIR@|libdir=@CMAKE_INSTALL_FULL_LIBDIR@|' $out/src/loader/openxr.pc.in
+        '';
+      };
+    in [
       "-DOpenGL_GL_PREFERENCE=LEGACY"
       "-DOPENMW_USE_SYSTEM_RECASTNAVIGATION=1"
       "-DFETCHCONTENT_SOURCE_DIR_OPENXR=${openxr-sdk}"
@@ -121,14 +94,19 @@ in
       "-DOPENMW_OSX_DEPLOYMENT=ON"
     ];
 
+    qtWrapperArgs = [ ''
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ pkgs.libglvnd pkgs.libuuid ]}
+    ''];
+
     # If not set, OSG plugin .so files become shell scripts on Darwin.
     dontWrapQtApps = pkgs.stdenv.hostPlatform.isDarwin;
 
-    runtimeDependencies = with pkgs; [
-      libuuid
-      libGL
-      mesa
-    ];
+    #runtimeDependencies = with pkgs; [
+    #  libuuid
+    #  libGL
+    #  libglvnd
+    #  mesa
+    #];
 
     installPhase = /*sh*/ ''
       runHook preInstall
@@ -136,20 +114,12 @@ in
       ### Run the default install phase provided by CMake
       cmake --build . --target install
 
-
-      ### Think the dir is already made by cmake?
-      #mkdir -p "$out/bin"
       ### Copy the openmw_vr binary to the output directory.
       install -m755 ./openmw_vr $out/bin
 
       runHook postInstall
     '';
 
-    #postFixup = /*sh*/ ''
-    #  omwvrExe="$out/bin/openmw_vr"
-    #  origRpath="$(patchelf --print-rpath "$omwvrExe")"
-    #  patchelf --set-rpath "${lib.makeLibraryPath [ libGL libglvnd libuuid ]}:$origRpath" "$omwvrExe"
-    #  '';
 
     meta = with lib; {
       description = "Unofficial VR open source engine reimplementation of the game Morrowind";
